57ec8555c629b58fe85277bc24f532e05228d77a
Vinicius Marques <vinicius.marques.silva_storm@prestador.globo>
2022-09-30 19:03:09 -0300

    feat: create gatilho views

---
+++ src/views/configuracoes/gatilhos/components/delete-gatilho.vue
@@ -0,0 +1,127 @@
+<template>
+    <v-dialog
+        :value="show"
+        @input="close"
+        max-width="600px"
+        persistent
+    >
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Apagar gatilho</span>
+          <v-spacer></v-spacer>
+          <v-btn
+              color="primary"
+              text
+              icon
+              @click="close"
+              :disabled="isDeleting"
+          >
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="mt-8">
+
+          <div>
+            Deseja realmente apagar esse gatilho? Esta ação não pode ser desfeita.
+          </div>
+
+          <v-alert
+            v-if="hasError"
+            outlined
+            type="error"
+            v-text="error"
+            class="ma-0"
+          ></v-alert>
+
+        </v-card-text>
+
+        <v-card-actions>
+          <div v-if="isDeleting">
+            <v-progress-circular indeterminate color="primary"></v-progress-circular>
+            <span class="ml-4">Apagando...</span>
+          </div>
+          <v-spacer></v-spacer>
+          <div class="mb-4 mr-2">
+            <v-btn
+                color="blue darken-1"
+                text
+                @click="close"
+                :disabled="isDeleting"
+            >
+              Cancelar
+            </v-btn>
+            <v-btn
+                depressed
+                color="primary"
+                @click="confirmDelete"
+                :disabled="isDeleting"
+            >
+              Apagar
+            </v-btn>
+          </div>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { app, TYPES } from "@/core/common/container";
+import { CreateOrUpdateGatilhoState } from "@/core/gatilho/presentation/states/create-or-update-gatilho.state";
+import { DeleteGatilhoController } from "@/core/gatilho/presentation/controllers/delete-gatilho.controller";
+import { GatilhoEntity } from "@/core/gatilho/domain/entities/gatilho.entity";
+
+@Component({})
+export default class DeleteGatilho extends Vue {
+  private controller = app.make<DeleteGatilhoController>(TYPES.DeleteGatilhoController);
+  private localState: CreateOrUpdateGatilhoState = this.controller.state;
+
+  @Prop() show!: boolean;
+  @Prop() item!: GatilhoEntity;
+
+  async confirmDelete() {
+    await this.controller.deleteGatilho(this.item.id);
+    this.close();
+  }
+
+  get isDeleting(): boolean {
+    return this.localState.kind === 'DeletingGatilhoState';
+  }
+
+  get hasError(): boolean {
+    return this.localState.kind === 'ErrorDeleteGatilhoState';
+  }
+
+  get error(): string | null {
+    if (this.hasError) {
+      return this.localState.error;
+    }
+    return null;
+  }
+
+  close() {
+    if (this.isDeleting) {
+      return;
+    }
+    this.$emit('close');
+  }
+
+  changeState(state) {
+    this.localState = state;
+  }
+
+  created() {
+    this.controller.subscribe(this.changeState);
+  }
+
+  beforeDestroy() {
+    this.controller.unsubscribe(this.changeState);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/configuracoes/gatilhos/index.vue
+++ src/views/configuracoes/gatilhos/index.vue
@@ -1,13 +1,230 @@
 <template>
-  <div>Complemento de setup</div>
+  <v-container class="pa-8">
+
+    <v-row>
+      <v-col>
+        <v-text-field
+          v-model="searchTerm"
+          outlined
+          dense
+          label="Busca"
+          :disabled="isLoading"
+        ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-btn
+          outlined
+          color="primary"
+          style="height: 40px"
+          @click="search"
+          :disabled="isLoading"
+        >Buscar</v-btn>
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col>
+        <div class="d-flex">
+          <v-spacer></v-spacer>
+          <v-btn
+              color="primary"
+              style="height: 40px"
+              @click="createItem"
+          >Novo</v-btn>
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-data-table
+        :headers="headers"
+        :items="localState.items"
+        class="elevation-1"
+        hide-default-footer
+        :loading="isLoading"
+        loading-text="Buscando gatilhos..."
+        no-data-text="Nenhum gatilho encontrado"
+    >
+
+      <template v-slot:top>
+        <delete-gatilho
+            :item="itemToDelete"
+            :show="showDeleteDialog"
+            @close="closeDeleteDialog"
+        ></delete-gatilho>
+        <create-or-update-gatilho
+            :item="itemToUpdate"
+            :show="showCreateOrUpdateDialog"
+            @close="closeCreateOrUpdateDialog"
+        ></create-or-update-gatilho>
+      </template>
+
+      <template v-slot:[`item.ativo`]="{ item }">
+        <v-switch
+            small
+            class="mr-2"
+            :input-value="item.ativo"
+            dense
+            @change="() => toggleAtivo(item, !item.ativo)"
+        >
+          mdi-pencil
+        </v-switch>
+      </template>
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-icon
+            small
+            class="mr-2"
+            @click="() => updateItem(item)"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+            small
+            @click="() => deleteItem(item)"
+        >
+          mdi-delete
+        </v-icon>
+      </template>
+    </v-data-table>
+
+    <v-row v-if="showPagination" justify="end" class="mt-2">
+      <v-col v-if="isLoading">
+        <v-progress-circular indeterminate color="primary"></v-progress-circular>
+        <span class="ml-4">Buscando...</span>
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col cols="4">
+        <v-pagination
+            :value="pagination.page"
+            @input="changePage"
+            :length="pagination.pageCount"
+        ></v-pagination>
+      </v-col>
+    </v-row>
+
+  </v-container>
 </template>
 
 <script lang="ts">
+import { app } from "@/core/common/container";
+import { ListGatilhoController } from "@/core/gatilho/presentation/controllers/list-gatilho.controller";
+import { TYPES } from "@/core/common/providers/types";
 import { Component, Vue } from "vue-property-decorator";
+import { ListGatilhoState } from "@/core/gatilho/presentation/states/list-gatilho.state";
+import { GatilhoEntity } from "@/core/gatilho/domain/entities/gatilho.entity";
+import DeleteGatilho from "@/views/configuracoes/gatilhos/components/delete-gatilho.vue";
+import CreateOrUpdateGatilho from "@/views/configuracoes/gatilhos/components/create-or-update-gatilho.vue";
 
 @Component({
+  components: { CreateOrUpdateGatilho, DeleteGatilho },
 })
-export default class SetupAddOnView extends Vue {
+export default class ListGatilho extends Vue {
+  private listGatilhoController: ListGatilhoController = app.make<ListGatilhoController>(TYPES.ListGatilhoController);
+  private localState: ListGatilhoState = this.listGatilhoController.state;
+
+  searchTerm = '';
+
+  itemToUpdate!: GatilhoEntity = null;
+  itemToDelete!: GatilhoEntity = null;
+
+  showDeleteDialog = false;
+  showCreateOrUpdateDialog = false;
+
+  get pagination() {
+    return {
+      page: this.localState.page,
+      itemsPerPage: this.localState.itemsPerPage,
+      pageCount: this.localState.pageCount,
+    }
+  }
+
+  get showPagination(): boolean{
+    return this.pagination.pageCount > 1;
+  }
+
+  async search(): Promise<void> {
+    await this.listGatilhoController.loadGatilhoList({
+      search: this.searchTerm,
+      page: 1,
+    });
+  }
+
+  closeDeleteDialog() {
+    this.itemToDelete = {};
+    this.showDeleteDialog = false;
+  }
+
+  closeCreateOrUpdateDialog() {
+    this.itemToUpdate = null;
+    this.showCreateOrUpdateDialog = false;
+  }
+
+  deleteItem(item) {
+    this.itemToDelete = item;
+    this.showDeleteDialog = true;
+  }
+
+  get isLoading(): boolean {
+    return this.localState.kind === 'LoadingListGatilhoState';
+  }
+
+  get headers() {
+    return [
+      {
+      text: 'NOME',
+        align: 'start',
+        sortable: false,
+        value: 'nome',
+        width: '70%',
+      },
+      {
+        text: 'ATIVO',
+        value: 'ativo',
+        sortable: false,
+        width: '15%',
+      },
+      {
+        text: '',
+        value: 'actions',
+        sortable: false,
+        width: '15%',
+      },
+    ];
+  }
+
+  private async changePage(page: number): Promise<void> {
+    await this.listGatilhoController.goToPage(page);
+  }
+
+  private async toggleAtivo(item: GatilhoEntity, value: boolean) {
+    await this.listGatilhoController.changeAtivo(item.id, value);
+  }
+
+  private createItem() {
+    this.itemToUpdate = null;
+    this.showCreateOrUpdateDialog = true;
+  }
+
+  private updateItem(item: GatilhoEntity) {
+    this.itemToUpdate = item;
+    this.showCreateOrUpdateDialog = true;
+  }
+
+  private created() {
+    this.listGatilhoController.subscribe(this.updateState);
+    this.listGatilhoController.resetState();
+  }
+
+  private updateState(newState: ListGatilhoState) {
+    this.$nextTick(() => {
+      this.localState = newState;
+    });
+  }
+
+  public mounted() {
+    this.listGatilhoController.loadGatilhoList();
+  }
+
+  private beforeDestroy() {
+    this.listGatilhoController.unsubscribe(this.updateState);
+  }
 }
 </script>
 
